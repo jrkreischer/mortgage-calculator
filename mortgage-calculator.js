5958fd7c57e52e024c74aa24040890bf0e3f4a5e
john <john@thinkthrive.com>
2017-10-02 16:10:48 -0500

    Clear previous error message after passing validation. Changed calculator div to encapsulate entire calculator.

--- mortgage-calculator.js
+++ mortgage-calculator.js
@@ -1,19 +1,19 @@
 
 var mortgageCalculator = (function() {
   // Inputs
-  var calculator = document.getElementById('calculator');
-  var years = document.getElementById('years');
-  var interestRate = document.getElementById('interest-rate');
-  var loanAmount = document.getElementById('loan-amount');
-  var annualTax = document.getElementById('annual-tax');
-  var annualInsurance = document.getElementById('annual-insurance');
-  var calculate = document.getElementById('calculate');
+  var calculator = document.getElementById('mortgage-calculator');
+  var years = document.querySelector('#mortgage-calculator #years');
+  var interestRate = document.querySelector('#mortgage-calculator #interest-rate');
+  var loanAmount = document.querySelector('#mortgage-calculator #loan-amount');
+  var annualTax = document.querySelector('#mortgage-calculator #annual-tax');
+  var annualInsurance = document.querySelector('#mortgage-calculator #annual-insurance');
+  var calculate = document.querySelector('#mortgage-calculator #calculate');
   // Outputs
-  var error = document.getElementById('error');
-  var principlePlusInterest = document.getElementById('principal-plus-interest');
-  var monthlyTax = document.getElementById('monthly-tax');
-  var monthlyInsurance = document.getElementById('monthly-insurance');
-  var monthlyPayment = document.getElementById('monthly-payment');
+  var error = document.querySelector('#mortgage-calculator #error');
+  var principlePlusInterest = document.querySelector('#mortgage-calculator #principal-plus-interest');
+  var monthlyTax = document.querySelector('#mortgage-calculator #monthly-tax');
+  var monthlyInsurance = document.querySelector('#mortgage-calculator #monthly-insurance');
+  var monthlyPayment = document.querySelector('#mortgage-calculator #monthly-payment');
   // Vars for calculation
   var n, i, p, t, ins;
 
@@ -63,7 +63,7 @@ var mortgageCalculator = (function() {
       var re = /^\d+(?:\.\d{1,2})?$/; // Regex to check for valid number
       var reDecPercent = /^(\.\d{1,2})?$/; // Regex for decimal percentage
       var good = 0; // Counter for validated inputs
-      var inputs = document.querySelectorAll('#calculator input[type=text]');
+      var inputs = document.querySelectorAll('#mortgage-calculator input[type=text]');
 
       // Loop text inputs, compare with regex, update status(good)
       for (var i = 0; i < inputs.length; i++) {
@@ -71,6 +71,7 @@ var mortgageCalculator = (function() {
           good++;
           if (inputs[i].style.borderColor == 'red') {
             inputs[i].style.borderColor = 'initial';
+            error.innerHTML = '&nbsp;';
           }
         } else {
           good--;
